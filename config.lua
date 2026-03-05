958ecb3aa510e3c74ed198f86d459cc8a8d910b0
Mato_T <matevz888@gmail.com>
2026-03-05 17:34:05 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,50 @@
+Config = {}
+
+Config.NPCLokacija = {
+    model = "s_m_y_marine_03",
+    coords = vector4(221.59, -813.59, 30.56, -20.0),
+    text = "LOKACIJE"
+}
+
+Config.Lokacije = {
+    {
+        ime = "NOVA FFA ARENA COSTUM",
+        coords = vector3(-1739.22, 157.87, 69.17),
+        slika = "images/arena.png"
+    },
+    {
+        ime = "Glavna banka",
+        coords = vector3(244.05, 216.59, 106.29),
+        slika = "images/banka.png"
+    },
+    {
+        ime = "Mala banka",
+        coords = vector3(-350.87, -48.53, 49.04),
+        slika = "images/mala_banka.png"
+    },
+    {
+        ime = "Baza podatkov",
+        coords = vector3(-1078.46, -249.55, 44.02),
+        slika = "images/baza.png"
+    },
+    {
+        ime = "FBI",
+        coords = vector3(112.76, -753.37, 45.75),
+        slika = "images/fbi.png"
+    },
+    {
+        ime = "Kitajc",
+        coords = vector3(-1224.56, -906.02, 12.33),
+        slika = "images/kitajc.png"
+    },
+    {
+        ime = "Trgovinca",
+        coords = vector3(373.875, 325.896, 103.566),
+        slika = "images/trgovinca.png"
+    },
+    {
+        ime = "Draguljarna",
+        coords = vector3(-629.01, -235.73, 38.10),
+        slika = "images/draguljarna.png"
+    }
+}
