2a7a715a10016441e19ec4c1d75a32b1139ad810
Mato_T <matevz888@gmail.com>
2026-03-13 11:41:32 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mato'
+description 'FFA Menu'
+version '1.1.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/images/*.png'
+}
